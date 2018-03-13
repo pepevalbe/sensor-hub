337f96a0ee97e43f2fd0493142d66ca8d09735bd
pepevalbe <pepevalbe@gmail.com>
2018-03-13 23:24:36 +0100

    Set time zone to utc at startup

--- src/main/java/com/pepe/sensor/SensorHubApplication.java
+++ src/main/java/com/pepe/sensor/SensorHubApplication.java
@@ -1,5 +1,7 @@
 package com.pepe.sensor;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -9,6 +11,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableAsync
 public class SensorHubApplication {
+
+    @PostConstruct
+    public void init() {
+        // Setting Application Time Zone to UTC
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SensorHubApplication.class, args);
