8f38ea48ec55fd6b1ca2e2b1b7e17815c77d9c1a
Pepe <pepevalbe@gmail.com>
2018-03-14 16:19:14 +0100

    Fixed repository class

--- src/main/java/com/pepe/sensor/repository/ConfigVariableRepository.java
+++ src/main/java/com/pepe/sensor/repository/ConfigVariableRepository.java
@@ -2,11 +2,11 @@ package com.pepe.sensor.repository;
 
 import com.pepe.sensor.persistence.ConfigVariable;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ConfigVariableRepository extends Repository<ConfigVariable, Long> {
+public interface ConfigVariableRepository extends CrudRepository<ConfigVariable, Long> {
 
     @Query("select x.varValue from ConfigVariable x where x.varKey = ?1")
     String getValueByKey(String varKey);
