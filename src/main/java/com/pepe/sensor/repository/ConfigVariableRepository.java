4c3612792d62b31195a8f6ccc8228a1bad5f0826
Pepe <pepevalbe@gmail.com>
2018-03-14 14:47:58 +0100

    Added @RepositoryRestResource

--- src/main/java/com/pepe/sensor/repository/ConfigVariableRepository.java
+++ src/main/java/com/pepe/sensor/repository/ConfigVariableRepository.java
@@ -2,9 +2,11 @@ package com.pepe.sensor.repository;
 
 import com.pepe.sensor.persistence.ConfigVariable;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface ConfigVariableRepository extends CrudRepository<ConfigVariable, Long> {
+@RepositoryRestResource
+public interface ConfigVariableRepository extends Repository<ConfigVariable, Long> {
 
     @Query("select x.varValue from ConfigVariable x where x.varKey = ?1")
     String getValueByKey(String varKey);
--- src/main/java/com/pepe/sensor/repository/TemporaryTokenRepository.java
+++ src/main/java/com/pepe/sensor/repository/TemporaryTokenRepository.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.pepe.sensor.repository;
 
 import com.pepe.sensor.persistence.TemporaryToken;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-
+@RepositoryRestResource
 public interface TemporaryTokenRepository extends CrudRepository<TemporaryToken, Long> {
-    
+
 }
