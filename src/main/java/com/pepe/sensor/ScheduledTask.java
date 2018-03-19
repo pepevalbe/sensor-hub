ae11baa30f5231c9311939563552baec879b15f8
Pepe <pepevalbe@gmail.com>
2018-03-19 11:26:49 +0100

    removed unnecesary keep awake code

--- src/main/java/com/pepe/sensor/ScheduledTask.java
+++ src/main/java/com/pepe/sensor/ScheduledTask.java
@@ -1,13 +1,7 @@
 package com.pepe.sensor;
 
-import com.pepe.sensor.repository.ConfigVariableRepository;
-import java.io.IOException;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Component
@@ -15,22 +9,6 @@ public class ScheduledTask {
 
     @Autowired
     private RestClient restClient;
-
-    @Autowired
-    ConfigVariableRepository configVariableRepository;
-
-    // This keeps heroku server awake
-    @Scheduled(fixedRate = 3000000) // Every 50 minutes
-    public void keepAwake() {
-        try {
-            URL obj = new URL(configVariableRepository.getValueByKey("APP_BASE_URL")); // get application url
-            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-            con.setRequestMethod("GET");    // optional default is GET
-            int responseCode = con.getResponseCode();   // make requeset
-        } catch (IOException ex) {
-            Logger.getLogger(ScheduledTask.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
 
     @Scheduled(fixedRate = 7200000, initialDelay = 1000) // Every 2 hours
     public void postTempHumidity() throws Exception {
