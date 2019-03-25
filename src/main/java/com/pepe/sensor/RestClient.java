5c26ca6f4de04c5d193b318eaa99b2c5f3d861ca
pepevalbe <pepevalbe@gmail.com>
2019-03-25 22:20:59 +0100

    Fix startup exceptions

--- src/main/java/com/pepe/sensor/RestClient.java
+++ src/main/java/com/pepe/sensor/RestClient.java
@@ -2,14 +2,13 @@ package com.pepe.sensor;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.pepe.sensor.dto.DoorEventDTO;
-import com.pepe.sensor.dto.SensorReadingDTO;
-import com.pepe.sensor.dto.TempHumidityDTO;
 import com.pepe.sensor.controller.DoorEventRestController;
 import com.pepe.sensor.controller.SensorReadingRestController;
 import com.pepe.sensor.controller.TempHumidityRestController;
-import com.pepe.sensor.repository.PersonRepository;
-import java.util.concurrent.ThreadLocalRandom;
+import com.pepe.sensor.dto.DoorEventDTO;
+import com.pepe.sensor.dto.SensorReadingDTO;
+import com.pepe.sensor.dto.TempHumidityDTO;
+import com.pepe.sensor.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -19,11 +18,13 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @Service
 public class RestClient {
 
     private final RestTemplate restTemplate;
-    private final PersonRepository personRepository;
+    private final UserService userService;
 
     @Value("${pepe-sensores.app_base_url}")
     private String APP_BASE_URL;
@@ -32,15 +33,17 @@ public class RestClient {
     private String WEATHER_URL;
 
     @Autowired
-    public RestClient(RestTemplateBuilder restTemplateBuilder,
-            PersonRepository personRepository,
-            ProjectConstants projectConstants) {
+    public RestClient(RestTemplateBuilder restTemplateBuilder, UserService userService, ProjectConstants projectConstants) {
         restTemplate = restTemplateBuilder.build();
-        this.personRepository = personRepository;
+        this.userService = userService;
     }
 
     @Async
     public void postTempHumidity() throws Exception {
+
+        if (WEATHER_URL.isEmpty()) {
+            return;
+        }
         ResponseEntity<String> response = restTemplate.getForEntity(WEATHER_URL, String.class);
         ObjectMapper mapper = new ObjectMapper();
         JsonNode root = mapper.readTree(response.getBody());
@@ -48,16 +51,16 @@ public class RestClient {
         TempHumidityDTO temphumidityDTO = new TempHumidityDTO();
         temphumidityDTO.setTemperature(Float.parseFloat(main.path("temp").asText()));
         temphumidityDTO.setHumidity(Float.parseFloat(main.path("humidity").asText()));
-        temphumidityDTO.setToken(personRepository.getOne("user").getToken());
+        temphumidityDTO.setToken(userService.getUserProfile("user").getToken());
         HttpEntity<TempHumidityDTO> request = new HttpEntity<>(temphumidityDTO);
         restTemplate.postForObject(APP_BASE_URL + TempHumidityRestController.PUBLIC_TEMPHUMIDITY_URL, request, TempHumidityDTO.class);
     }
 
     @Async
     public void postDoorEvent() throws Exception {
-        Thread.sleep(ThreadLocalRandom.current().nextLong(1, 100) * 60 * 1000L);    // Seleeps 1-100 minutes
+        Thread.sleep(ThreadLocalRandom.current().nextLong(1, 100) * 60 * 1000L);    // Sleeps 1-100 minutes
         DoorEventDTO doorEventDTO = new DoorEventDTO();
-        doorEventDTO.setToken(personRepository.getOne("user").getToken());
+        doorEventDTO.setToken(userService.getUserProfile("user").getToken());
         HttpEntity<DoorEventDTO> request = new HttpEntity<>(doorEventDTO);
         restTemplate.postForObject(APP_BASE_URL + DoorEventRestController.PUBLIC_DOOREVENT_URL, request, DoorEventDTO.class);
     }
@@ -68,7 +71,7 @@ public class RestClient {
         sensorReadingDTO.setValue1(ThreadLocalRandom.current().nextFloat());
         sensorReadingDTO.setValue2((float) ThreadLocalRandom.current().nextDouble(-100, 100));
         sensorReadingDTO.setValue3((float) ThreadLocalRandom.current().nextGaussian());
-        sensorReadingDTO.setToken(personRepository.getOne("user").getToken());
+        sensorReadingDTO.setToken(userService.getUserProfile("user").getToken());
         HttpEntity<SensorReadingDTO> request = new HttpEntity<>(sensorReadingDTO);
         restTemplate.postForObject(APP_BASE_URL + SensorReadingRestController.PUBLIC_GENERICSENSOR_URL, request, SensorReadingDTO.class);
     }
