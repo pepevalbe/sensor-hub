e0dee1b152c1aea054e89491662836449273aae8
pepevalbe <pepevalbe@gmail.com>
2020-04-11 14:48:31 +0200

    Removed unnecessary method

--- src/main/java/com/pepe/sensor/controller/UserController.java
+++ src/main/java/com/pepe/sensor/controller/UserController.java
@@ -18,15 +18,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.net.URI;
 import java.security.Principal;
 
+import static com.pepe.sensor.VarKeeper.USER_REGISTRY_ENABLED_KEY;
+
 @Slf4j
 @Controller
 @AllArgsConstructor
 public class UserController {
 
-	private static final String PUBLIC_USERNAME_URL = "/public/username";
-	private static final String USER_USERPROFILE_URL = "/user/userprofile";
+	private static final String PUBLIC_USERPROFILE_URL = "/public/userprofile";
 	private static final String PUBLIC_CREATEUSER_URL = "/public/createuser";
-	private static final String PUBLIC_MODIFYUSER_URL = "/public/modifyuser";
+	private static final String USER_MODIFYUSER_URL = "/user/modifyuser";
 	private static final String USER_REGENERATEPERSONALTOKEN_URL = "/user/regeneratepersonaltoken";
 	private static final String PUBLIC_GENERATEPASSWORDTOKEN_URL = "/public/generatepasswordtoken";
 	private static final String PUBLIC_RESETPASSWORD_URL = "/public/resetpassword";
@@ -35,28 +36,12 @@ public class UserController {
 	private final UserService userService;
 
 	/**
-	 * Get current logged username
-	 *
-	 * @param principal Automatically filled when user is logged
-	 * @return Logged username or 403 forbidden if not logged
-	 */
-	@GetMapping(PUBLIC_USERNAME_URL)
-	public ResponseEntity<String> getUsername(Principal principal) {
-
-		if (principal == null) {
-			return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-		} else {
-			return ResponseEntity.ok(userService.getUserByUsername(principal.getName()).getUsername());
-		}
-	}
-
-	/**
 	 * Get current logged user profile
 	 *
 	 * @param principal Automatically filled when user is logged
 	 * @return Logged user (Person object) or 403 (forbidden)if not logged
 	 */
-	@GetMapping(USER_USERPROFILE_URL)
+	@GetMapping(PUBLIC_USERPROFILE_URL)
 	public ResponseEntity<PersonDto> getUserProfile(Principal principal) {
 
 		if (principal == null) {
@@ -76,7 +61,7 @@ public class UserController {
 	@PostMapping(PUBLIC_CREATEUSER_URL)
 	public ResponseEntity<String> createUser(@RequestBody PersonDto userDTO) {
 
-		if (varKeeper.isUserRegistryEnabled()) {
+		if (Boolean.parseBoolean(varKeeper.get(USER_REGISTRY_ENABLED_KEY))) {
 			if (userService.getUserByUsername(userDTO.getUsername()) != null) {
 				return ResponseEntity.status(HttpStatus.CONFLICT).body("El nombre de usuario ya existe, por favor elija otro.");
 			} else if (userService.getUserByEmail(userDTO.getEmail()) != null) {
@@ -97,7 +82,7 @@ public class UserController {
 	 * @param userDTO User to modify in database
 	 * @return 409 (conflict) in case username or email already exists, or 200 if user is successfully saved
 	 */
-	@PostMapping(PUBLIC_MODIFYUSER_URL)
+	@PostMapping(USER_MODIFYUSER_URL)
 	public ResponseEntity<String> modifyUser(@RequestBody PersonDto userDTO) {
 
 		if (userService.getUserByUsername(userDTO.getUsername()) != null) {
--- src/main/resources/static/public/js/loadnavbarandfooter.js
+++ src/main/resources/static/public/js/loadnavbarandfooter.js
@@ -1,19 +1,19 @@
 // Constants
-var GET_USERNAME_URL = "/public/username";
+var GET_USERPROFILE_URL = "/public/userprofile";
 var NAVBAR_URL = "/public/navbar.html";
 var FOOTER_URL = "/public/footer.html";
 
 // Load Navbar, footer and get username for navbar
 $(document).ready(function () {
     $("#navbarId").load(NAVBAR_URL, function () {
-        $.ajax({url: GET_USERNAME_URL,
+        $.ajax({url: GET_USERPROFILE_URL,
             error: function () {
                 $("#LoginItemId").attr("hidden", false);
                 $("#SignUpItemId").attr("hidden", false);
                 $("#UserItemId").attr("hidden", true);
             },
             success: function (data) {
-                $("#UserItemTextId").text("Usuario: " + data);
+                $("#UserItemTextId").text("Usuario: " + data.username);
                 $("#LoginItemId").attr("hidden", true);
                 $("#SignUpItemId").attr("hidden", true);
                 $("#UserItemId").attr("hidden", false);
