c7285d2518850d842a132aff7e21a2b1ac955b08
pepevalbe <pepevalbe@gmail.com>
2021-01-31 19:34:13 +0100

    Add active profile to test

--- src/test/java/com/pepe/sensor/SensorHubApplicationTests.java
+++ src/test/java/com/pepe/sensor/SensorHubApplicationTests.java
@@ -3,8 +3,10 @@ package com.pepe.sensor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@ActiveProfiles("development")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SensorHubApplicationTests {
